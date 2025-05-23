2c04639bfddabad588b8658edf3f3076fa55e972
Matt <2172212+TheEndIsNear@users.noreply.github.com>
2025-05-22 19:29:42 -0600

    initial commit

---
+++ lib/thermostat_nerves.ex
@@ -0,0 +1,18 @@
+defmodule ThermostatNerves do
+  @moduledoc """
+  Documentation for `ThermostatNerves`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ThermostatNerves.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/thermostat_nerves_test.exs
@@ -0,0 +1,8 @@
+defmodule ThermostatNervesTest do
+  use ExUnit.Case
+  doctest ThermostatNerves
+
+  test "greets the world" do
+    assert ThermostatNerves.hello() == :world
+  end
+end
