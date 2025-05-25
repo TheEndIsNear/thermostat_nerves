54c7c488b48d61e5d48974a8ba05931136406d1b
Matt <2172212+TheEndIsNear@users.noreply.github.com>
2025-05-24 20:12:21 -0600

    add the w1 rpi5

--- config/config.exs
+++ config/config.exs
@@ -12,6 +12,7 @@ Application.start(:nerves_bootstrap)
 # https://hexdocs.pm/nerves/advanced-configuration.html for details.
 
 config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
+config :nerves, :firmware, fwup_conf: "config/rpi/fwup.conf"
 
 # Set the SOURCE_DATE_EPOCH date for reproducible builds.
 # See https://reproducible-builds.org/docs/source-date-epoch/ for more information
